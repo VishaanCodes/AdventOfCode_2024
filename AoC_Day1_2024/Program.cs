a71b96b8fdfd083a4b1448d42367e0d143ce72e6
Vishaan Vohra <vohravishaan@gmail.com>
2024-12-02 13:43:16 +0000

    Add project files.

---
+++ AoC_Day1_2024/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day1_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Input_AoC_D1_2024.txt";
+            List<int> left = new List<int>(),
+                      right = new List<int>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string leftSubstring = line.Substring(0, 5);
+                    left.Add(int.Parse(leftSubstring.Trim()));
+                    string rightSubstring = line.Substring(8);
+                    right.Add(int.Parse(rightSubstring.Trim()));
+                }
+            }
+            left.Sort();
+            right.Sort();
+            int temp = 0, difference = 0;
+            for (int i = 0; i < left.Count; i++)
+            {
+                temp = Math.Abs(left[i] - right[i]);
+                difference += temp;
+            }
+            Console.WriteLine(difference);
+
+            int similarity = 0;
+            for (int i = 0; i < left.Count; i++)
+            {
+                temp = 0;
+                for (int j = 0; j < right.Count; j++)
+                {
+                    if (left[i] == right[j])
+                    {
+                        temp++;
+                    }
+                }
+                similarity += (left[i] * temp);
+            }
+            Console.WriteLine(similarity);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AoC_Day2_2024/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day2_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Input_AoC_D2_2024.txt";
+            int safe = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                bool first = false;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] strings = line.Split(' ');
+                    for (int i = 0; i < strings.Length - 1; i++)
+                    {
+                        if (int.Parse(strings[i]) > int.Parse(strings[i + 1]))
+                        {
+                            first = true;
+                        }
+                        else
+                        {
+                            first = false;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < strings.Length - 1; i++)
+                    {
+                        if (int.Parse(strings[i]) < int.Parse(strings[i + 1]))
+                        {
+                            first = true;
+                        }
+                        else
+                        {
+                            first = false;
+                            break;
+                        }
+                    }
+                    if (first)
+                    {
+                        for (int i = 0; i < strings.Length - 1; i++)
+                        {
+                            int current = int.Parse(strings[i].Trim());
+                            int next = int.Parse(strings[i + 1].Trim());
+                            for (int diff = -3; diff <= 3; diff++)
+                            {
+                                if (current + diff == next)
+                                {
+                                    safe++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(safe);
+            Console.ReadLine();
+        }
+    }
+}
